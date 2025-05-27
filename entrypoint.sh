e81004e56dc51520b8cd9056ae44ae0b04e51703
PHPDave <davelhs03@yahoo.com>
2025-05-27 17:30:42 -0400

    additional config changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,14 @@ set -e
 # Load environment variables from .env file
 export $(grep -v '^#' /var/www/html/.env | xargs)
 
+cp /etc/odbc.ini.template /etc/odbc.ini
+
 # Substitute placeholders in the template to generate actual odbc.ini
-envsubst < /etc/odbc.ini.template > /etc/odbc.ini
+sed -i "s/{{DB_SYSTEM}}/${DB_SYSTEM}/g" /etc/odbc.ini
+sed -i "s/{{DB_DATABASE}}/${DB_DATABASE}/g" /etc/odbc.ini
+sed -i "s/{{DB_USER}}/${DB_USER}/g" /etc/odbc.ini
+sed -i "s/{{DB_PASS}}/${DB_PASS}/g" /etc/odbc.ini
+sed -i "s/{{DB_LIBRARY_LIST}}/${DB_LIBRARY_LIST}/g" /etc/odbc.ini
 
 # Fix permissions if needed
 chown -R www-data:www-data /var/www/html/logs
