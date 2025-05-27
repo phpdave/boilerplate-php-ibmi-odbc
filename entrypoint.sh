07e1504340de46a1ed94630f5f7ee0c5a65e730a
PHPDave <davelhs03@yahoo.com>
2025-05-27 17:37:18 -0400

    additional changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -e
 
-# Load environment variables from .env file
-export $(grep -v '^#' /var/www/html/.env | xargs)
-
+# Copy template to actual odbc.ini
 cp /etc/odbc.ini.template /etc/odbc.ini
 
-# Substitute placeholders in the template to generate actual odbc.ini
-sed -i "s/{{DB_SYSTEM}}/${DB_SYSTEM}/g" /etc/odbc.ini
-sed -i "s/{{DB_DATABASE}}/${DB_DATABASE}/g" /etc/odbc.ini
-sed -i "s/{{DB_USER}}/${DB_USER}/g" /etc/odbc.ini
-sed -i "s/{{DB_PASS}}/${DB_PASS}/g" /etc/odbc.ini
-sed -i "s/{{DB_LIBRARY_LIST}}/${DB_LIBRARY_LIST}/g" /etc/odbc.ini
+# Replace placeholders with environment variables
+sed -i "s|{{DB_SYSTEM}}|${DB_SYSTEM}|g" /etc/odbc.ini
+sed -i "s|{{DB_DATABASE}}|${DB_DATABASE}|g" /etc/odbc.ini
+sed -i "s|{{DB_USER}}|${DB_USER}|g" /etc/odbc.ini
+sed -i "s|{{DB_PASS}}|${DB_PASS}|g" /etc/odbc.ini
+sed -i "s|{{DB_LIBRARY_LIST}}|${DB_LIBRARY_LIST}|g" /etc/odbc.ini
 
-# Fix permissions if needed
 chown -R www-data:www-data /var/www/html/logs
 
-# Run Apache
 exec apache2-foreground
