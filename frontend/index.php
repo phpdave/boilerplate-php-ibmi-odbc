707737189897c53a5e9ae9c3a600d0282078a709
PHPDave <davelhs03@yahoo.com>
2025-05-27 17:39:38 -0400

    links to the other pages

--- frontend/index.php
+++ frontend/index.php
@@ -1,2 +1,4 @@
 <?php
-echo "hi in index.php";
+echo "hi in index.php<br/><br/>";
+echo "<a href='https://localhost:8080/odbctest.php'>Go to ODBCTEST</a><br/><br/>";
+echo "<a href='https://localhost:8080/info.php'>Go to PHPINFO</a>";
